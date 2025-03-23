a2eda83fd213c3bee2a9f1ff3297627abfc20c32
ppppphrt <phavida.r@ku.th>
2025-03-23 18:18:21 +0700

    added two new classes

---
+++ Enemy.py
@@ -0,0 +1,14 @@
+
+
+class Enemy:
+
+    def __init__(self):
+        self.position = 0,0
+        self.speed = 0
+        self.enemy_encounter = 0
+
+    def move_enemy(self):
+        pass
+
+    def detect_player(self):
+        pass
---
+++ Leaderboard.py
@@ -0,0 +1,13 @@
+
+
+class Leaderboard:
+
+    def __init__(self):
+
+        self.file_path = []
+
+    def save_score(self, player_name, score, time_taken):
+        pass
+
+    def load_score(self):
+        pass
